70d789e8db25925cf1a372e9e98a782f57ff3ef5
austin noh <noh1austin@gmail.com>
2019-11-27 15:35:12 -0500

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true})); // this is what you need to use body parser
 
 app.get("/", function(req, res) {
-    res.send("Hello World.")
+    res.sendFile(__dirname + "/index.html");
   })
 
+app.post("/", function(req, res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var added = num1 + num2;
+    res.send("The calculation is " + added);
+
+    console.log(req.body);
+})
 app.listen("3000", function(){
     console.log("Litening at port 3000")
 })
