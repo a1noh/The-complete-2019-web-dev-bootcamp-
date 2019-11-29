7c54091a0c15953ee5b1c013e05bcb292dead5de
austin noh <noh1austin@gmail.com>
2019-11-29 17:02:04 -0500

    bitcoint ticker

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
   })
 
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+})
+
 app.post("/", function(req, res){
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
@@ -15,6 +19,14 @@ app.post("/", function(req, res){
 
     console.log(req.body);
 })
+
+app.post("/bmiCalculator", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight / (height * height);
+    res.send("Your bmi is " + bmi); 
+})
+
 app.listen("3000", function(){
     console.log("Litening at port 3000")
 })
