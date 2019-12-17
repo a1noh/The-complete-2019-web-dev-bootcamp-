c024afdd1dba13d07dd65972082056725398b72c
austin noh <noh1austin@gmail.com>
2019-12-17 17:11:20 +0900

    mv ignroe

--- Newsletter-Signup/api.js
+++
@@ -1,9 +0,0 @@
-module.exports.getApi = function (){
-
-    return 'https://us4.api.mailchimp.com/3.0/lists/acb2e3618e'
-};
-
-module.exports.getid = function (){
-
-    return "noh1 ad4c49d6c4a37096c803e4d5a0991abd-us4"
-};
